f1ff1dd4e710ced0b78abe913446b51d27bf60a9
Veronica Bustabad <vbustabad@gmail.com>
2018-10-13 21:40:40 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-// Code Goes Here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class MovieBrowser extends React.Component {
+  render() {
+    const currentPlayingTitle = 'Mad Max: Fury Road';
+    const childrenWithExtraProp = React.Children.map(this.props.children, child => {
+      return React.cloneElement(child, {
+        isPlaying: child.props.title === currentPlayingTitle
+      });
+    });
+
+    return (
+      <div className="movie-browser">
+        {childrenWithExtraProp}
+      </div>
+    );
+  }
+}
